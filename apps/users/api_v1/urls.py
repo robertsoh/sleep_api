5bdf493725c45ba1625f605eef07c0c39c7bb500
Robertson Ore <robertson.ore@gmail.com>
2018-07-15 23:15:08 -0500

    Agrega buscar usuario

--- apps/users/api_v1/urls.py
+++ apps/users/api_v1/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from rest_framework.authtoken import views
 
-from apps.users.api_v1.views import UsersCreateAPIView
+from apps.users.api_v1.views import UsersCreateAPIView, SearchUserListView
 
 urlpatterns = [
     url(r'^register$',
         UsersCreateAPIView.as_view(),
         name='register_user'),
-    url(r'^login$', views.obtain_auth_token)
+    url(r'^login$', views.obtain_auth_token, name='login'),
+    url(r'^searchUsers$', SearchUserListView.as_view(), name='search_user')
 ]
--- apps/users/api_v1/views.py
+++ apps/users/api_v1/views.py
@@ -1,7 +1,24 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from apps.users.api_v1.serializers import UserSerializer
+from apps.users.models import User
 
 
 class UsersCreateAPIView(CreateAPIView):
     serializer_class = UserSerializer
+
+
+class SearchUserListView(ListAPIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        username = self.request.query_params.get('username')
+        if username:
+            qs = User.objects.filter(username__icontains=username)
+        else:
+            qs = User.objects.none()
+        return qs
